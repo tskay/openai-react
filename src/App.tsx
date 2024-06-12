eb79a63c82aba9c55f3d06e7b5a379750bce6bfa
tskay <52343388+tskay@users.noreply.github.com>
2024-06-13 03:23:32 +0800

    qwe2

--- src/App.tsx
+++ src/App.tsx
@@ -4,75 +4,52 @@ import Chat from './components/Chat';
 import './App.css';
 import logo from './logo.png'; // Adjust the path to where your actual logo isc:\Users\User\Dropbox\Bugcrusher Dropbox\openai-react\src\logo.png
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faWarning, faHouse, faTablet, faAngleDown} from '@fortawesome/free-solid-svg-icons'
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header id="site-header">
-        <div className="header-inner">
-          <div className="site-logo">
-          <img src={logo} alt="Logo" />
-          </div>
-          <div className="menu-wrap">
-          </div>
-        </div>
-        <div className="header-bottom">
-          <div className="flex-row">
-          <nav>
-                    <ul className="menu-text">
-                        <li>
-                            <a href="https://tskay.github.io/allstar/index.html">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Info
-                  <span>
-                    <i className="fa fa-angle-down" aria-hidden="true"></i>
-                  </span>
-                  <ul>
-                    <li>
-                      <a href="https://tskay.github.io/allstar/road-sign.html">Road Sign Malaysia</a>
-                        </li>
-                        <li>
-                            <a href="https://tskay.github.io/allstar/special-day.html">Special Days</a>
-                        </li>
-                        <li>
-                            <a href="https://tskay.github.io/allstar/data.html">bar chart data</a>
-                        </li>
-                    </ul>
-                    </a>
-                    </li>
-                    <li>
-                        <a href="https://tskay.github.io/allstar/research-pillar.html">Research</a>
-                    </li>
-                    <li>
-                        <a href="https://tskay.github.io/allstar/SDG/sdglandpage.html">SDG</a>
-                    </li>
-                    <li>
-                        <a href="#">Learn
+        <header className="header-duuuurian">
+        <div className="header-menu-duuuurian">
+          <div className="header-duuuurian flex-row-duuuurian container-duuuurian">
+            <div className="flex-col-duuuurian flex-right-duuuurian">
+              <ul className="nav-duuuurian nav-right-duuuurian nav-spacing-duuuurian">
+                <li>
+                  <a href="index.html"><button className="hover-button">
+                    <FontAwesomeIcon icon={faHouse} />
+                    <span className="button-text">Home</span>
+                </button></a>
+                </li>
+                <li>
+                  <a href="negatives.html"><button className="hover-button">
+                  <FontAwesomeIcon icon={faWarning} />
+                    <span className="button-text">Negative Effect</span>
+                </button></a>
+                </li>
+                <li>
+                  <a href="kts_test.html"><button className="hover-button">
+                  <FontAwesomeIcon icon={faTablet} />
+                    <span className="button-text">Test</span>
+                </button></a>
+                </li>
+                <li>
+                  <a href="#">SDGs
                     <span>
-                      <i className="fa fa-angle-down" aria-hidden="true"></i>
+                    <FontAwesomeIcon icon={faAngleDown} />
                     </span>
                     <ul>
                       <li>
-                        <a href="https://tskay.github.io/allstar/car_go_brrrrrrrr.html">Drive Simulator</a>
+                        <a href="" className="assd">SDG Info</a>
                       </li>
                       <li>
-                          <a href="https://tskay.github.io/allstar/quiz.html">Quiz</a>
-                      </li>
-                      <li>
-                          <a href="https://tskay.github.io/allstar/card_game.html">Card Game</a>
-                      </li>
-                      </ul>
-                      </a>
-                      </li>
-                      <li>
-                        <a href="cta.html">Call to Action</a>
-                      </li>
-                      <li>
-                        <a href="https://chatbot-euq3.onrender.com/">Chat</a>
+                        <a href="" className="assd">SDG Games</a>
                       </li>
                     </ul>
-                    
-                </nav>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
