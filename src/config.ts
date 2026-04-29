eb08f47c0d6e87a48b8bbba71d9bf48e972abf43
tskay <52343388+tskay@users.noreply.github.com>
2026-04-30 01:49:19 +0800

    x

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Central app configuration (single source of truth).
-export const WEBSITE_URL = "https://ygk4tt-5000.csb.app";
+export const WEBSITE_URL = "https://stackblitzstarterswgk5o8kl-l2j5--8080--4c73681d.local-credentialless.webcontainer.io/";
 
