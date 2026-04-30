cb6f68f9eae3f4e6157a4d07a400867861c75e1f
tskay <52343388+tskay@users.noreply.github.com>
2026-04-30 21:47:33 +0800

    x

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Central app configuration (single source of truth).
-export const WEBSITE_URL = "https://stackblitzstarterswgk5o8kl-l2j5--8080--4c73681d.local-credentialless.webcontainer.io/";
+export const WEBSITE_URL = "https://stackblitz.com/~/github.com/tskay/BugCrusher26_R2";
 
